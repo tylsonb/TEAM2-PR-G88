33c9e715970ecd5447a12abadb671e123ab00e1b
tylsonb <bellotj@gmail.com>
2024-08-20 22:50:24 -0400

    comentarios codigo JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,11 @@
+
+// leea el contenido del DOM 
 document.addEventListener("DOMContentLoaded", function(event){
     const formulario = document.querySelector("form")
-
+// previene compotamiento por defecto
     formulario.addEventListener("submit", function(event){
         event.preventDefault()
+// Valore ingresado por el usuario
 
         const name = event.target.varName.value
         const album = event.target.varAlbum.value
@@ -10,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function(event){
         const disco = event.target.varDisco.value
         const genero = event.target.varGenero.value
 
+// crea los elementos de la tabla
         const fila = document.createElement("tr")
 
         const Celdaname = document.createElement("td")
@@ -18,21 +22,23 @@ document.addEventListener("DOMContentLoaded", function(event){
         const Celdadisco = document.createElement("td")
         const Celdagenero = document.createElement("td")
 
+// asignar  valores el elemento td
         Celdaname.textContent = name
         Celdaalbum.textContent = album
         Celdaartista.textContent = artista
         Celdadisco.textContent = disco
         Celdagenero.textContent =  genero
 
+// añade el valor a la celda en la fila
         fila.appendChild(Celdaname)
         fila.appendChild(Celdaalbum)
         fila.appendChild(Celdaartista)
         fila.appendChild(Celdadisco)
         fila.appendChild(Celdagenero)
-
+// dibuja la tabla en el DOM
         const tabla = document.querySelector("tbody")
         tabla.appendChild(fila);
-
+// limpia los campps ingresados por el usuario
         formulario.reset();
 
 
